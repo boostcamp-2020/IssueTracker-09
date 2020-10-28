6ffc06f0a7e8cd79feee21df7996e77942268be1
Jae-Woo Joo <wnwodn4293@naver.com>
2020-10-28 17:27:12 +0900

    feat: 라벨 조회 API 구현
    - 라벨 조회 컨트롤러 로직 구현
    - 라벨 조회 서비스 로직 구현

--- backend/src/controller/label.js
+++ backend/src/controller/label.js
@@ -9,7 +9,14 @@ module.exports = {
       res.status(403).json(false);
     }
   },
-  get: (req, res) => {},
+  get: async (req, res) => {
+    const result = await labelService.get(req.user);
+    if (result) {
+      res.status(201).json(result);
+    } else {
+      res.status(403).json(false);
+    }
+  },
   update: (req, res) => {},
   remove: (req, res) => {},
 };
--- backend/src/service/label.js
+++ backend/src/service/label.js
@@ -4,8 +4,15 @@ module.exports = {
   create: async ({ color, title, content }) => {
     try {
       const result = await Label.create({ color: color, title: title });
-      console.log(result);
       return true;
+    } catch (error) {
+      return false;
+    }
+  },
+  get: async () => {
+    try {
+      const result = await Label.findAll();
+      return result;
     } catch (error) {
       return false;
     }
