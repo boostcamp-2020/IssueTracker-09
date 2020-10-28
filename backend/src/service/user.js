128539d5cab74d31bb4cd66af74ded3746a5413f
Jae-Woo Joo <wnwodn4293@naver.com>
2020-10-28 15:08:08 +0900

    feat: 유저 서비스 기능 구현
    - apple 로그인 비지니스로직 구현

--- backend/src/service/user.js
+++ backend/src/service/user.js
@@ -1,4 +1,5 @@
 const { createJWT } = require('../utils/jwt');
+const { User } = require('../model');
 
 module.exports = {
   gitHubLogin: (user) => {
@@ -6,6 +7,23 @@ module.exports = {
       const { id, image, name } = user.dataValues;
       const jwtToken = createJWT(id);
       return { token: jwtToken, image, name };
+    } catch (error) {
+      return false;
+    }
+  },
+  iosAppleLogin: async (data) => {
+    try {
+      const { code, name } = data;
+      const [result] = await User.findOrCreate({
+        where: { user_code: code },
+        defaults: {
+          user_code: code,
+          name: name,
+        },
+      });
+      const { id } = result.dataValues;
+      const jwtToken = createJWT(id);
+      return { token: jwtToken };
     } catch (error) {
       return false;
     }
