ab8a854026bc41f277fec51f4b1804b916ef6381
Jae-Woo Joo <wnwodn4293@naver.com>
2020-10-28 20:07:39 +0900

    refactor: 라벨 컨트롤러 함수명 수정 및 에러처리
    - 함수명 create, read, update, remove 로 변경
    - 서버 에러를 잡기위해 try catch 문을 사용
    - 상태코드 수정 및 추가

--- backend/src/controller/label.js
+++ backend/src/controller/label.js
@@ -2,35 +2,51 @@ const labelService = require('../service/label');
 
 module.exports = {
   create: async (req, res) => {
-    const result = await labelService.create(req.body);
-    if (result) {
-      res.status(201).json(true);
-    } else {
-      res.status(403).json(false);
+    try {
+      const result = await labelService.create(req.body);
+      if (!result.error) {
+        res.status(201).json(result);
+      } else {
+        res.status(403).json(result.error);
+      }
+    } catch (error) {
+      res.status(500).json({ error });
     }
   },
-  get: async (req, res) => {
-    const result = await labelService.get(req.user);
-    if (result) {
-      res.status(200).json(result);
-    } else {
-      res.status(403).json(false);
+  read: async (req, res) => {
+    try {
+      const result = await labelService.read();
+      if (!result.error) {
+        res.status(200).json(result);
+      } else {
+        res.status(403).json(result.error);
+      }
+    } catch (error) {
+      res.status(500).json({ error });
     }
   },
   update: async (req, res) => {
-    const result = await labelService.update(req.body);
-    if (result) {
-      res.status(200).json(true);
-    } else {
-      res.status(403).json(false);
+    try {
+      const result = await labelService.update(req.body);
+      if (!result.error) {
+        res.status(200).json(result);
+      } else {
+        res.status(403).json(false);
+      }
+    } catch (error) {
+      res.status(500).json(false);
     }
   },
   remove: async (req, res) => {
-    const result = await labelService.remove(req.params);
-    if (result) {
-      res.status(200).json(true);
-    } else {
-      res.status(403).json(false);
+    try {
+      const result = await labelService.remove(req.params);
+      if (!result.error) {
+        res.status(200).json(result);
+      } else {
+        res.status(403).json(false);
+      }
+    } catch (error) {
+      res.status(500).json(false);
     }
   },
 };
