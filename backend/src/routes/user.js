bb22223bacb9d5516c7e034a2be0d47a7934bd14
Jae-Woo Joo <wnwodn4293@naver.com>
2020-10-28 15:06:36 +0900

    feat: user 라우팅 설정
    - apple 로그인 라우팅 설정
    - getUser 라우팅 설정

--- backend/src/routes/user.js
+++ backend/src/routes/user.js
@@ -2,13 +2,15 @@
 const express = require('express');
 const passport = require('passport');
 const githubAuth = require('./middleware/git-auth');
+const jwtAuth = require('./middleware/jwt-auth');
 
 // Controller
-const { gitHubLogin } = require('../controller/user');
+const User = require('../controller/user');
 
 const router = express.Router();
 
 router.get('/github', passport.authenticate('github'));
-router.get('/github/callback', githubAuth, gitHubLogin);
-
+router.get('/github/callback', githubAuth, User.gitHubLogin);
+router.post('/apple', User.iosAppleLogin);
+router.get('/', jwtAuth, User.getUser);
 module.exports = router;
