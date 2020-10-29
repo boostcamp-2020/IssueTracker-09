84f9406fb18b9a29ac23bfd8950b05e3629adbf1
Jae-Woo Joo <wnwodn4293@naver.com>
2020-10-29 18:02:59 +0900

    feat: 이슈 생성 서비스 로직 구현
    이슈 생성 서비스 로직 구현
    이슈 생성 후 라벨과 어사이너 추가
    하나라도 실패시 rollback 설정

--- backend/src/service/issue.js
+++ backend/src/service/issue.js
@@ -1,6 +1,30 @@
 const Model = require('../model');
 
 module.exports = {
+  create: async ({ milestoneId, assigneeId, labelId, title, content, id }) => {
+    if (!title) {
+      return { error: '정보가 부족합니다' };
+    }
+    const transaction = await Model.sequelize.transaction();
+    try {
+      const issue = await Model.Issue.create(
+        {
+          title,
+          content,
+          user_id: id,
+          milestone_id: milestoneId,
+        },
+        { transaction }
+      );
+      await issue.addLabels(labelId, { transaction });
+      await issue.addUsers(assigneeId, { transaction });
+      transaction.commit();
+      return true;
+    } catch (error) {
+      await transaction.rollback();
+      return { error };
+    }
+  },
   read: async () => {
     const issues = await Model.Issue.findAll({
       group: 'Comments.id',
