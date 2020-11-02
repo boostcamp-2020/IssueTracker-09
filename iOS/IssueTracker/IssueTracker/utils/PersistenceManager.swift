4be7da9f11ba7ca209325cdba8d1ddeb3ef6d714
Seungeon-Kim <seungeon1221@gmail.com>
2020-11-02 15:39:39 +0900

    feat: 유저 로그인 토큰 저장을 위한 키체인 클래스

---
+++ iOS/IssueTracker/IssueTracker/utils/PersistenceManager.swift
@@ -0,0 +1,29 @@
+//
+//  PersistenceManager.swift
+//  IssueTracker
+//
+//  Created by Seungeon Kim on 2020/11/02.
+//
+
+import Foundation
+import KeychainSwift
+
+
+enum Keys: String {
+    case token
+}
+
+class PersistenceManager {
+    static let shared: PersistenceManager = PersistenceManager()
+    
+    private init() { }
+    
+    @discardableResult
+    func save(_ token: String, forKey: Keys) -> Bool {
+        return KeychainSwift().set(token, forKey: forKey.rawValue)
+    }
+    
+    func load(forKey: Keys) -> String? {
+        return KeychainSwift().get(forKey.rawValue)
+    }
+}
