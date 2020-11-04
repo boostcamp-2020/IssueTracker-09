257e890354deedc003de1fffb0ad5c471cff05ac
Seungeon-Kim <seungeon1221@gmail.com>
2020-11-04 13:13:07 +0900

    feat: api service class 추가
    - local issue api
    - milestone request issue api
    - milestone api

--- iOS/IssueTracker/IssueTracker/ViewControllers/Issue/LocalIssueService.swift
+++ iOS/IssueTracker/IssueTracker/ViewControllers/Issue/LocalIssueService.swift
@@ -8,29 +8,24 @@
 import Foundation
 
 class LocalIssueService: IssueService {
-    let issue = Issue(number: 0,
-                      title: "레이블 목록 보기 구현",
-                      description: "레이블 전체 목록을 볼 수 있어야 한다 2줄까지 보입니다.",
-                      sprint: "",
-                      labels: [""],
-                      status: .opened)
+    let issue = Issue(id: 0, title: "레이블 목록 보기 구현", content: "레이블 전체 목록을 볼 수 있어야 한다 2줄까지 보입니다.", isOpened: true, milestone: nil, commentCount: 0, user: [], label: [])
     
     internal lazy var issues = {
-       return Issues([issue, issue, issue, issue, issue, issue, issue, issue, issue, issue, issue, issue])
+        return Issues(issue: [issue, issue, issue, issue, issue, issue, issue, issue, issue, issue, issue, issue])
     }()
     
     subscript(at: Int) -> Issue {
         get{
-            issues.issues[at]
+            issues.issue[at]
         }
     }
     
     func remove(indexOf: Int) {
-        issues.issues.remove(at: indexOf)
+        issues.issue.remove(at: indexOf)
     }
     
     var count: Int {
-        return issues.issues.count
+        return issues.issue.count
     }
     
 //
