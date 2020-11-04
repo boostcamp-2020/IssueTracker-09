af275762bcaeea3f826aa3c6d60ab2c985bf2d5c
박은식 <qkrdmstlr3@naver.com>
2020-11-04 17:19:41 +0900

    feat: user store 추가
    - user store를 contextAPI로 구현

---
+++ client/src/stores/userStore.js
@@ -0,0 +1,66 @@
+import React, { createContext, useReducer } from 'react';
+import PropTypes from 'prop-types';
+
+const initialState = {
+  name: undefined,
+  image: undefined,
+};
+
+export const LOGIN_USER = 'LOGIN_USER';
+export const LOGOUT_USER = 'LOGOUT_USER';
+export const GET_USER = 'GET_USER';
+
+const userReducer = (state, action) => {
+  switch (action.type) {
+    case LOGIN_USER: {
+      return state;
+    }
+    case LOGOUT_USER: {
+      return state;
+    }
+    case GET_USER: {
+      return state;
+    }
+    default: {
+      return state;
+    }
+  }
+};
+
+const UserContext = createContext();
+
+const UserProvider = ({ children }) => {
+  const [userState, dispatch] = useReducer(userReducer, initialState);
+
+  const userAction = {
+    loginUser: () => {
+      dispatch({ type: LOGIN_USER });
+    },
+    logoutUser: () => {
+      dispatch({ type: LOGOUT_USER });
+    },
+    getUser: () => {
+      dispatch({ type: GET_USER });
+    },
+  };
+
+  return (
+    // eslint-disable-next-line react/jsx-filename-extension
+    <UserContext.Provider value={{ userState, userAction }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
+
+UserProvider.propTypes = {
+  children: PropTypes.node,
+};
+
+export { UserContext, UserProvider };
+
+// 예시
+// import React, { useContext } from 'react';
+// import { UserContext } from '../store/userStore';
+// const {
+//   userState: { name },
+// } = useContext(UserContext);
