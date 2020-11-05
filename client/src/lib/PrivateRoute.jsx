ab832a5920da276ccbb06cf9357e7683c01a5daa
Jae-Woo Joo <wnwodn4293@naver.com>
2020-11-05 18:12:32 +0900

    feat: private 라우트 구현(미완성)
    - 로그인 상태를 판별하여 로그인시 원하는 페이지, 아닐시 login페이지로 이동
    - 현재 로그인 페이지로 돌아가는 기능 X

--- client/src/lib/PrivateRoute.jsx
+++ client/src/lib/PrivateRoute.jsx
@@ -2,7 +2,7 @@
 /* eslint-disable react/jsx-props-no-spreading */
 /* eslint-disable react/prop-types */
 import React, { useContext, useEffect } from 'react';
-import { Redirect, Route } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 import { UserContext } from '../stores/userStore';
 
 const PrivateRoute = ({ component: Component, ...rest }) => {
@@ -11,24 +11,13 @@ const PrivateRoute = ({ component: Component, ...rest }) => {
     userAction: { getUser },
   } = useContext(UserContext);
 
-  // useEffect(() => {
-  //   async function getHandler() {
-  //     await getUser();
-  //   }
-
-  //   if (!name) {
-  //     getHandler();
-  //   }
-  // }, []);
-
-  return (
-    <Route
-      {...rest}
-      render={(props) =>
-        name ? <Component {...props} /> : <Redirect to="/login" />
-      }
-    />
-  );
+  useEffect(async () => {
+    await getUser();
+  }, []);
+  if (name) {
+    return <Route {...rest} render={(props) => <Component {...props} />} />;
+  }
+  return <div>hello</div>;
 };
 
 export default PrivateRoute;
