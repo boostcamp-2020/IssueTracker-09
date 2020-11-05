86f0ef5bbd491868e6f05f0f9563c1d792597357
Seungeon-Kim <seungeon1221@gmail.com>
2020-11-05 16:04:54 +0900

    feat: 레이아웃 확인을 위한 마일스톤 데이터 추가

--- iOS/IssueTracker/IssueTracker/ViewControllers/Milestone/MilestoneCacheService.swift
+++ iOS/IssueTracker/IssueTracker/ViewControllers/Milestone/MilestoneCacheService.swift
@@ -27,7 +27,12 @@ class MilestoneCacheService: MilestoneService {
     
     func reloadData() {
         networkService.fetchMilestones { [weak self] result in
-            self?.milestones = (try? result.get().milestone) ?? []
+//            self?.milestones = (try? result.get().milestone) ?? []
+            let milestone1 = Milestone(id: 1, title: "milestone1", content: "첫번째 마일스톤", deadline: "2020.11.03", isOpened: true)
+            let milestone2 = Milestone(id: 2, title: "milestone2", content: "두번째 마일스톤", deadline: "2020.11.11", isOpened: true)
+            let milestone3 = Milestone(id: 3, title: "milestone3", content: "세번째 마일스톤", deadline: "2020.11.22", isOpened: true)
+            
+            self?.milestones = [milestone1, milestone2, milestone3]
             self?.delegate?.didDataLoaded()
         }
     }
