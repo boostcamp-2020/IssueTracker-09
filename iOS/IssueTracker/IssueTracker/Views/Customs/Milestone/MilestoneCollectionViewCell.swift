71ef8e40f44044c8281ef15da541fa15b3b40a0a
Seungeon-Kim <seungeon1221@gmail.com>
2020-11-05 16:04:03 +0900

    reafc: 마일스톤 목록 페이지 레이아웃 수정

--- iOS/IssueTracker/IssueTracker/Views/Customs/Milestone/MilestoneCollectionViewCell.swift
+++ iOS/IssueTracker/IssueTracker/Views/Customs/Milestone/MilestoneCollectionViewCell.swift
@@ -7,6 +7,23 @@
 
 import UIKit
 
+@IBDesignable
 class MilestoneCollectionViewCell: UICollectionViewCell {
+    @IBInspectable
+    var cornerRadius: CGFloat {
+        get { return layer.cornerRadius }
+        set { layer.cornerRadius = newValue }
+    }
     
+    @IBInspectable
+    var borderWidth: CGFloat {
+        get { return layer.borderWidth }
+        set { layer.borderWidth = newValue }
+    }
+    
+    @IBInspectable
+    var borderColor: UIColor? {
+        get { return UIColor.init(cgColor: layer.borderColor!) }
+        set { layer.borderColor = newValue?.cgColor }
+    }
 }
