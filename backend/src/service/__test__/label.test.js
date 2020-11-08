cbd51d86338d8e4cfeb50eeeeda4584186408d52
박은식 <qkrdmstlr3@naver.com>
2020-11-08 17:17:46 +0900

    test: milestone service 테스트 코드 작성
    - milestone service의 crud테스트 코드 작성

--- backend/src/service/__test__/label.test.js
+++ backend/src/service/__test__/label.test.js
@@ -56,12 +56,12 @@ describe('update label service 테스트', () => {
     expect(typeof labelService.update).toBe('function');
   });
   it('리턴 타입이 object인가', async () => {
-    Label.update.mockReturnValue(labelResult);
+    Label.update.mockReturnValue([1]);
     const result = await labelService.update(labelResult);
     expect(typeof result).toBe('object');
   });
   it('잘못된 정보는 에러를 반환하는가', async () => {
-    Label.update.mockReturnValue(labelResult);
+    Label.update.mockReturnValue([1]);
     const result = await labelService.update();
     expect(result).toEqual({ error: '정보가 부족합니다' });
   });
@@ -78,12 +78,12 @@ describe('remove label service 테스트', () => {
     expect(typeof labelService.remove).toBe('function');
   });
   it('리턴 타입이 object인가', async () => {
-    Label.destroy.mockReturnValue(labelResult);
+    Label.destroy.mockReturnValue(1);
     const result = await labelService.remove(labelResult);
     expect(typeof result).toBe('object');
   });
   it('잘못된 정보는 에러를 반환하는가', async () => {
-    Label.destroy.mockReturnValue(labelResult);
+    Label.destroy.mockReturnValue(1);
     const result = await labelService.remove();
     expect(result).toEqual({ error: '없는 id값 입니다' });
   });
