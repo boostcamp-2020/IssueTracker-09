38596f2f826773ff6d067623c5fc0820e321f99c
Seungeon-Kim <seungeon1221@gmail.com>
2020-11-10 01:35:20 +0900

    feat: 이슈 상세 코디네이터 추가

--- iOS/IssueTracker/IssueTracker/Coordinators/Roots/Main/Issue/IssueDetailCoordinator.swift
+++ iOS/IssueTracker/IssueTracker/Coordinators/Roots/Main/Issue/IssueDetailCoordinator.swift
@@ -5,4 +5,35 @@
 //  Created by Seungeon Kim on 2020/11/09.
 //
 
-import Foundation
+import UIKit
+
+class IssueDetailCoordinator: Coordinator {
+    private enum StoryboardName: String {
+        case IssueDetail, IssueBottomSheet
+    }
+    private(set) var window: UIWindow
+    private(set) var childCoordinators: [String : Coordinator] = [:]
+    private weak var parent: UIViewController?
+    
+    private var issue: Issue
+    
+    required init(window: UIWindow, parent: UIViewController, issue: Issue) {
+        self.window = window
+        self.parent = parent
+        self.issue = issue
+    }
+    
+    func start() {
+        let storyBoard = UIStoryboard(name: StoryboardName.IssueDetail.rawValue, bundle: nil)
+        let issueDeatailViewController = storyBoard.instantiateViewController(
+            identifier: "IssueDetailViewController",
+            creator: {
+                coder in
+                return IssueDetailViewController(coder: coder)
+            })
+        
+        issueDeatailViewController.service = IssueDetailCacheService(issue: issue, delegate: issueDeatailViewController)
+         
+        (parent as? UINavigationController)?.pushViewController(issueDeatailViewController, animated: true)
+    }
+}
