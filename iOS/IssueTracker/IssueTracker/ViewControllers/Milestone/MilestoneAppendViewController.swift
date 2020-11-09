d4e8ddea998eb44081ffb1bbf4dd6815f4fd6f6d
KYHyeon <run190822@gmail.com>
2020-11-09 16:54:32 +0900

    fix: 마일스톤 추가 화면에서 키보드가 화면을 가리는 문제 수정

--- iOS/IssueTracker/IssueTracker/ViewControllers/Milestone/MilestoneAppendViewController.swift
+++ iOS/IssueTracker/IssueTracker/ViewControllers/Milestone/MilestoneAppendViewController.swift
@@ -15,12 +15,18 @@ class MilestoneAppendViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setupTitle()
+        addKeyboardObserver()
     }
     
     private func setupTitle() {
         titleInputField.setTitle("제목")
         deadlineInputField.setTitle("완료\n날짜")
         descriptionInputField.setTitle("설명")
+    }
+    
+    private func addKeyboardObserver() {
+        NotificationCenter.default.addObserver(self, selector: #selector(willKeyboardShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(willKeyboardHide), name: UIResponder.keyboardWillHideNotification, object: nil)
     }
     
     @IBAction func didCloseButtonTapped(_ sender: UIButton) {
@@ -35,5 +41,21 @@ class MilestoneAppendViewController: UIViewController {
     
     @IBAction func didSaveButtonTapped(_ sender: UIButton) {
         //TODO
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
+    
+    @objc func willKeyboardShow(_ notification: NSNotification) {
+        if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
+            UIView.animate(withDuration: 0.3, animations: {
+                self.view.transform = CGAffineTransform(translationX: 0, y: -keyboardSize.height / 2)
+            })
+        }
+    }
+    
+    @objc func willKeyboardHide(_ notification: NSNotification) {
+        view.transform = .identity
     }
 }
