618184fc1f2ad1372bb279798ffae4b9759c0be5
Seungeon-Kim <seungeon1221@gmail.com>
2020-11-09 16:07:54 +0900

    feat: Issue 정보 수정을 위한 프로토콜 추가

---
+++ iOS/IssueTracker/IssueTracker/ViewControllers/Issue/IssueEditService.swift
@@ -0,0 +1,21 @@
+//
+//  IssueDetailService.swift
+//  IssueTracker
+//
+//  Created by Seungeon Kim on 2020/11/09.
+//
+
+import Foundation
+
+protocol IssueEditService {
+    var issue: Issue { get set }
+    func willEditAssignee(old: [User], new: [User])
+    func willEditLabels(old: [Label], new: [Label])
+    func willEditMilestone(new: Milestone)
+}
+
+protocol IssueEditServiceDelegate: AnyObject {
+    func didAssigneeLoaded(isSuccess: Bool)
+    func didLabelsLoaded(isSuccess: Bool)
+    func didMilestoneLoaded(isSuccess: Bool)
+}
