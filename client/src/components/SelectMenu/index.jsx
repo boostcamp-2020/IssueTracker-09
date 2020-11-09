cb13da7c1fa0603c905838053b9f8c9c823efcbd
Jae-Woo Joo <wnwodn4293@naver.com>
2020-11-09 15:36:07 +0900

    feat: SelectMenu 컴포넌트 수정
    - onclick 이벤트 대신 Link 컴포넌트를 활용하여 URL 변경

--- client/src/components/SelectMenu/index.jsx
+++ client/src/components/SelectMenu/index.jsx
@@ -1,17 +1,16 @@
 /* eslint-disable react/self-closing-comp */
 
 import React, { useContext } from 'react';
-import { IssueContext } from '../../stores/issueStore';
+import { Link } from 'react-router-dom';
 import { UsersContext } from '../../stores/usersStore';
 import { ListItem, Modal, Title, Image, Name, Div } from './styled';
+import makeSearch from '../../lib/make-search';
 
-const SelectMenu = () => {
+const SelectMenu = ({ title }) => {
   const {
     usersState: { users },
   } = useContext(UsersContext);
-  const {
-    issueAction: { getList },
-  } = useContext(IssueContext);
+
   return (
     <Modal>
       <Div>
@@ -19,10 +18,15 @@ const SelectMenu = () => {
       </Div>
       <Div>
         {users.map((item, index) => (
-          <ListItem onClick={() => getList(`author:${item.name}`)} key={index}>
-            <Image width="20px" height="20px" src={item.image} />
-            <Name>{item.name}</Name>
-          </ListItem>
+          <Link
+            to={makeSearch(`${title}:${item.name}`, 'author:123')}
+            key={index}
+          >
+            <ListItem>
+              <Image width="20px" height="20px" src={item.image} />
+              <Name>{item.name}</Name>
+            </ListItem>
+          </Link>
         ))}
       </Div>
     </Modal>
