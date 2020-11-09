6c0e597d4625fe0742101ac5f2efc3538eddeb51
Seungeon-Kim <seungeon1221@gmail.com>
2020-11-09 22:05:34 +0900

    chore: 그룹 구조 변경

--- iOS/IssueTracker/IssueTracker/Coordinators/Roots/Main/Issue/Filter/FilterCoordinator.swift
+++ iOS/IssueTracker/IssueTracker/Coordinators/Roots/Main/Issue/Filter/FilterCoordinator.swift
@@ -7,14 +7,14 @@
 
 import UIKit
 
-class FilterCoordinator: ChildCoordinator {
+class FilterCoordinator: Coordinator {
     private enum StoryboardName: String {
         case Filter
         case Search
     }
     
     private(set) var window: UIWindow
-    private(set) var childCoordinators: [String : ChildCoordinator] = [: ]
+    private(set) var childCoordinators: [String : Coordinator] = [: ]
     
     private weak var parent: UIViewController?
     private let navigationController = UINavigationController()
