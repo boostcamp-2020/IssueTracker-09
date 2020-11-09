28da5b7c9b04f7d24023247c2f626ac868eda59e
Seungeon-Kim <seungeon1221@gmail.com>
2020-11-09 22:08:45 +0900

    feat:  assignee 네트워크 서비스 객체 추가

---
+++ iOS/IssueTracker/IssueTracker/Networks/AssigneeNetworkService.swift
@@ -0,0 +1,33 @@
+//
+//  AssigneeNetworkService.swift
+//  IssueTracker
+//
+//  Created by Seungeon Kim on 2020/11/09.
+//
+
+import Foundation
+import Alamofire
+
+class AssigneeNetworkService: NetworkService {
+    enum Endpoint: String {
+        case users = "/user/users"
+    }
+    
+    func fetchAssignee(completion handler: @escaping (Result<Assignee, AFError>) -> Void) {
+        guard let url = URL(string: baseURL + Endpoint.users.rawValue),
+              let token = PersistenceManager.shared.load(forKey: .token) else {
+            return
+        }
+        
+        let headers: HTTPHeaders = [.authorization(bearerToken: token)]
+        
+        AF.request(url,
+                   method: .get,
+                   headers: headers)
+            .validate()
+            .responseDecodable(of: Assignee.self) { response in
+                handler(response.result)
+            }
+    }
+}
+
