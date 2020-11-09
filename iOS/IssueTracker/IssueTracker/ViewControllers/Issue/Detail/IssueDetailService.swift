6e6e273c9d33200a7402e49a21ef74a007583e57
Seungeon-Kim <seungeon1221@gmail.com>
2020-11-09 22:08:07 +0900

    feat: 이슈 상새 댓글 및 담당자 네트워크 서비스 프로토콜 추가

--- iOS/IssueTracker/IssueTracker/ViewControllers/Issue/Detail/IssueDetailService.swift
+++ iOS/IssueTracker/IssueTracker/ViewControllers/Issue/Detail/IssueDetailService.swift
@@ -10,8 +10,10 @@ import Foundation
 protocol IssueDetailService {
     var issue: Issue { get set }
     func requestComments()
+    func requestUsers()
 }
 
 protocol IssueDetailServiceDelegate: AnyObject {
     func didCommentsLoaded(comments: [Comment])
+    func didAssigneeLoaded(assignee: [User])
 }
