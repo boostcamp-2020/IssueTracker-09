a08f025e9ce8bf12e533a3acb9e4bac147b85bfb
Seungeon-Kim <seungeon1221@gmail.com>
2020-11-11 03:47:02 +0900

    feat: pangesture 함수 파일 분리

---
+++ iOS/IssueTracker/IssueTracker/ViewControllers/Issue/Detail/Bottom/IssueBottomSheetViewController+PanGesture.swift
@@ -0,0 +1,55 @@
+//
+//  IssueBottomSheetViewController+PanGesture.swift
+//  IssueTracker
+//
+//  Created by Seungeon Kim on 2020/11/11.
+//
+
+import UIKit
+
+extension IssueBottomSheetViewController: UIGestureRecognizerDelegate {
+    @objc func panGesture(_ recognizer: UIPanGestureRecognizer) {
+        let translation = recognizer.translation(in: self.view)
+        let velocity = recognizer.velocity(in: self.view)
+
+        let y = self.view.frame.minY
+        if (y + translation.y >= fullView) && (y + translation.y <= partialView) {
+            self.view.frame = CGRect(x: 0, y: y + translation.y, width: view.frame.width, height: view.frame.height)
+            recognizer.setTranslation(CGPoint.zero, in: self.view)
+        }
+        
+        if recognizer.state == .ended {
+            var duration =  velocity.y < 0 ? Double((y - fullView) / -velocity.y) : Double((partialView - y) / velocity.y )
+            
+            duration = duration > 1.3 ? 1 : duration
+            
+            UIView.animate(withDuration: duration, delay: 0.0, options: [.allowUserInteraction], animations: {
+                if  velocity.y >= 0 {
+                    self.view.frame = CGRect(x: 0, y: self.partialView, width: self.view.frame.width, height: self.view.frame.height)
+                } else {
+                    self.view.frame = CGRect(x: 0, y: self.fullView, width: self.view.frame.width, height: self.view.frame.height)
+                }
+                
+                }, completion: { [weak self] _ in
+                    if ( velocity.y < 0 ) {
+//                        self?.tableView.isScrollEnabled = true
+                    }
+            })
+        }
+    }
+    // Solution
+    func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+        let gesture = (gestureRecognizer as! UIPanGestureRecognizer)
+        let direction = gesture.velocity(in: view).y
+
+        let y = view.frame.minY
+        if (y == fullView && direction > 0) || (y == partialView) {
+//            tableView.isScrollEnabled = false
+        } else {
+//            tableView.isScrollEnabled = true
+        }
+        
+        return false
+    }
+    
+}
