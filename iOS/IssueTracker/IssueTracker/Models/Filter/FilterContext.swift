087192375fdabc069b519e474f7e394d64f26e8d
KYHyeon <run190822@gmail.com>
2020-11-11 00:55:51 +0900

    feat: 필터 상태를 가지고있는 모델을 싱글톤 클래스로 변경
    - 여러 곳에서 접근하기 편하게 하기위해서 변경

--- iOS/IssueTracker/IssueTracker/Models/Filter/FilterContext.swift
+++ iOS/IssueTracker/IssueTracker/Models/Filter/FilterContext.swift
@@ -7,10 +7,21 @@
 
 import Foundation
 
-struct FilterContext {
-    let condition: Int?
-    let writer: User?
-    let label: Label?
-    let milestone: Milestone?
-    let assignee: User?
+class FilterContext {
+    static var shared = FilterContext()
+    private init() {}
+    
+    var condition: Int? = nil
+    var writer: User? = nil
+    var label: Label? = nil
+    var milestone: Milestone? = nil
+    var assignee: User? = nil
+    
+    func clear() {
+        condition = nil
+        writer = nil
+        label = nil
+        milestone = nil
+        assignee = nil
+    }
 }
