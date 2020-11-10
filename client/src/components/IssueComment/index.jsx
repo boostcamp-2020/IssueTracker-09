12cddac262fc42dc90db1560e54d5b7e691ebe5e
Jae-Woo Joo <wnwodn4293@naver.com>
2020-11-10 18:07:16 +0900

    feat: 최초 랜더링시 데이터 가져오는 hook 구현
    - 최초 랜더링시 useEffect를 사용하여 API를 호출하여 데이터 받아오기
    - 받아온 데이터로 현재 상태를 바꿔줘 랜더링

--- client/src/components/IssueComment/index.jsx
+++ client/src/components/IssueComment/index.jsx
@@ -1,14 +1,19 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import { UserContext } from '../../stores/userStore';
 import { Container, FlexDiv, Image } from './styled';
 import NewComment from '../NewComment';
 import Comment from '../Comment';
+import getCommentAPI from '../../apis/comment';
 
-const IssueComment = ({ comments }) => {
+const IssueComment = ({ id }) => {
   const {
     userState: { name, image },
   } = useContext(UserContext);
-  const [commentList, setCommentList] = useState(comments);
+  const [commentList, setCommentList] = useState([]);
+  useEffect(async () => {
+    const result = await getCommentAPI(id);
+    setCommentList(result);
+  }, []);
   const addCommentHandler = (input) => {
     // api 호출 부분
     const result = {
