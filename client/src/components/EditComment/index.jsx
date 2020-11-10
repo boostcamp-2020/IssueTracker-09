87f41a1729a2fe532f9fb938098e166c9a163f23
Jae-Woo Joo <wnwodn4293@naver.com>
2020-11-10 17:48:57 +0900

    feat: 댓글 수정 컴포넌트 구현
    - 댓글 수정 컴포넌트 구현
    - 댓글 수정시 클라이언트 사이드 랜더링
    - 댓글 수정 API 연결 미흡

---
+++ client/src/components/EditComment/index.jsx
@@ -0,0 +1,22 @@
+/* eslint-disable react/button-has-type */
+import React, { useState } from 'react';
+import { Textarea, WriteBox } from './styled';
+
+const EditComment = ({ comment, action, update }) => {
+  const [inputValue, setInputvalue] = useState(comment);
+  return (
+    <WriteBox>
+      <div>write</div>
+      <Textarea
+        value={inputValue}
+        onChange={(e) => setInputvalue(e.target.value)}
+      />
+      <div>
+        <button onClick={() => action(false)}>CANCEL</button>
+        <button onClick={() => update(inputValue)}>Update comment</button>
+      </div>
+    </WriteBox>
+  );
+};
+
+export default EditComment;
