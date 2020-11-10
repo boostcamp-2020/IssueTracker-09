5392a755bcddffb0e41139a29f736161c0f9bffb
박은식 <qkrdmstlr3@naver.com>
2020-11-10 13:57:44 +0900

    feat: issue detail페이지 레이아웃
    - issue Detail페이지 레이아웃을 잡음

---
+++ client/src/components/IssueSidebar/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const IssueSidebar = () => {
+  return <div>hello</div>;
+};
+
+export default IssueSidebar;
--- client/src/pages/IssueDetail/index.jsx
+++ client/src/pages/IssueDetail/index.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import IssueHeader from '../../components/IssueHeader';
+import IssueComment from '../../components/IssueComment';
+import IssueSidebar from '../../components/IssueSidebar';
+import { Container, Top, Bottom } from './styled';
 
 const IssueDetail = () => {
-  return <div>hello</div>;
+  return (
+    <Container>
+      <Top>
+        <IssueHeader />
+      </Top>
+      <Bottom>
+        <IssueComment />
+        <IssueSidebar />
+      </Bottom>
+    </Container>
+  );
 };
 
 export default IssueDetail;
