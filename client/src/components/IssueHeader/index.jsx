59bab5e1ede925240262f377d745681dc5e15d3f
Jae-Woo Joo <wnwodn4293@naver.com>
2020-11-10 17:45:17 +0900

    feat: 이슈 상세보기 페이지 헤더 구현
    - 이슈 상세보기 페이지 헤더 구현
    - API 호출부분 미구현

--- client/src/components/IssueHeader/index.jsx
+++ client/src/components/IssueHeader/index.jsx
@@ -1,7 +1,70 @@
-import React from 'react';
+/* eslint-disable camelcase */
+/* eslint-disable react/button-has-type */
+import React, { useState } from 'react';
+import {
+  Container,
+  FlexDiv,
+  InputBox,
+  Div,
+  Span,
+  Icon,
+  IconBackground,
+} from './styled';
 
-const IssueHeader = () => {
-  return <div>hello</div>;
+const IssueHeader = ({ issue: { title, is_opened } }) => {
+  const [inputValue, setInputValue] = useState(title || '');
+  const [state, setState] = useState(false);
+  const [issueTitle, setIssueTitle] = useState(title || '');
+  const saveTitleHandler = () => {
+    // API 호출부?
+    if (true) {
+      setIssueTitle(inputValue);
+    }
+    setState(false);
+  };
+  const cancelTitleHandler = () => {
+    setInputValue(issueTitle);
+    setState(false);
+  };
+  return (
+    <Container>
+      <FlexDiv align="space-between">
+        <Div width="80%">
+          {state ? (
+            <InputBox
+              value={inputValue}
+              onChange={(e) => setInputValue(e.target.value)}
+            />
+          ) : (
+            <Span>{inputValue}</Span>
+          )}
+        </Div>
+        <Div>
+          {state ? (
+            <>
+              <button onClick={cancelTitleHandler}>CANCEL</button>
+              <button onClick={saveTitleHandler}>SAVE</button>
+            </>
+          ) : (
+            <button
+              onClick={() => {
+                setState(true);
+              }}
+            >
+              edit
+            </button>
+          )}
+        </Div>
+      </FlexDiv>
+      <FlexDiv>
+        <IconBackground isOpen={is_opened}>
+          <Icon>!</Icon>
+          {is_opened ? 'open' : 'close'}
+        </IconBackground>
+        <Div padding="5px">joojaewoo created this issue</Div>
+      </FlexDiv>
+    </Container>
+  );
 };
 
 export default IssueHeader;
