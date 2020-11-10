a5655e48e577293466f6747d76e52269f9602915
박은식 <qkrdmstlr3@naver.com>
2020-11-10 16:18:06 +0900

    feat: issue상세페이지일 떄 dropdown컴포넌트 설정
    - issue상세페이지일 때 dropdown컴포넌트에서 params로 판단하여서 dropdownitem컴포넌트를 렌더링시킨다

---
+++ client/src/components/DropdownItem/index.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import {
+  Modal,
+  Title,
+  ListItem,
+  DummyImage,
+  Name,
+  Image,
+  Color,
+} from './styled';
+
+const DropdownItem = ({ title, data, changeState, serverData }) => {
+  const clickHandler = (item) => {
+    changeState(item);
+  };
+
+  return (
+    <Modal>
+      <Title>{title}</Title>
+      {data?.map((item, index) => {
+        return (
+          <ListItem key={index} onClick={() => clickHandler(item)}>
+            {item.image === '' ? (
+              <>
+                <DummyImage />
+                <Name>{item.name}</Name>
+              </>
+            ) : item.image ? (
+              <>
+                <Image image={item.image} />
+                <Name>{item.name}</Name>
+              </>
+            ) : (
+              ''
+            )}
+            {item.color ? <Color color={item.color} /> : ''}
+            {item.title ? <Name>{item.title}</Name> : ''}
+            {serverData.some((d) => d.id === item.id) ? (
+              <div>v</div>
+            ) : (
+              <div> </div>
+            )}
+          </ListItem>
+        );
+      })}
+    </Modal>
+  );
+};
+
+export default DropdownItem;
