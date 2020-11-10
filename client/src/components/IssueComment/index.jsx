7c22d035cc442c1cdb919bc754fd7b05e7282b02
Jae-Woo Joo <wnwodn4293@naver.com>
2020-11-10 17:46:34 +0900

    feat: 이슈 댓글 컴포넌트 구현
    - 이슈 댓글 클라이언트 사이드 랜더링 구현
    - 이슈 댓글 생성 API 호출 미구현

--- client/src/components/IssueComment/index.jsx
+++ client/src/components/IssueComment/index.jsx
@@ -1,7 +1,44 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
+import { UserContext } from '../../stores/userStore';
+import { Container, FlexDiv, Image } from './styled';
+import NewComment from '../NewComment';
+import Comment from '../Comment';
 
-const IssueComment = () => {
-  return <div>hello</div>;
+const IssueComment = ({ comments }) => {
+  const {
+    userState: { name, image },
+  } = useContext(UserContext);
+  const [commentList, setCommentList] = useState(comments);
+  const addCommentHandler = (input) => {
+    // api 호출 부분
+    const result = {
+      timestamp: '2020-11-10T07:31:13.757Z',
+      id: 10,
+      content: input,
+      user_id: 1,
+      issue_id: 4,
+    };
+    if (result) {
+      result.User = {
+        id: result.user_id,
+        name,
+        image,
+      };
+      setCommentList([...commentList, result]);
+    }
+  };
+
+  return (
+    <Container>
+      {commentList?.map((comment, index) => (
+        <Comment comment={comment} key={index} />
+      ))}
+      <FlexDiv>
+        <Image image={image} />
+        <NewComment addEvent={addCommentHandler} closeEvent="123" />
+      </FlexDiv>
+    </Container>
+  );
 };
 
 export default IssueComment;
---
+++ client/src/components/IssueComment/styled.js
@@ -0,0 +1,36 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  width: 60%;
+`;
+
+export const FlexDiv = styled.div`
+  display: flex;
+`;
+
+export const InputBox = styled.input`
+  width: 80%;
+  height: 30px;
+  padding: 5px 1px;
+  font-size: 15px;
+`;
+
+export const Div = styled.div`
+  width: ${(props) => props.width};
+`;
+
+export const Span = styled.span`
+  width: 100%;
+  font-size: 60px;
+  font-weight: bolder;
+`;
+
+export const Image = styled.div`
+  background-image: url(${(props) => props.image});
+  background-size: cover;
+  border-radius: 10px;
+  display: block;
+  width: 30px;
+  height: 30px;
+  margin-right: 5px;
+`;
