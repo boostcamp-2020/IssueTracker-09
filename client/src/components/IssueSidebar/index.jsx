d3ba68dda598f5d6be1155264a4ca9f935c8d1a0
박은식 <qkrdmstlr3@naver.com>
2020-11-10 16:16:53 +0900

    feat: issue상세보기 페이지의 사이드바의 디자인
    - issue상세보기 페이지에서 사이드바의 메인 컴포넌트를 구현
    - cog는 아직 크기조절 실패로 붙이지 않았다

--- client/src/components/IssueSidebar/index.jsx
+++ client/src/components/IssueSidebar/index.jsx
@@ -1,7 +1,63 @@
 import React from 'react';
+import { Container, DropdownContainer } from './styled';
+import AssigneeContainer from './Assignee';
+import LabelContainer from './Label';
+import MilestoneContainer from './Milestone';
+
+const issue = {
+  Assignees: [
+    {
+      id: 1,
+      user_code: 'g46195613',
+      name: 'namee',
+      image: 'https://avatars2.githubusercontent.com/u/46195613?v=4',
+    },
+    {
+      id: 2,
+      user_code: 'g46195613',
+      name: 'namee2',
+      image: '',
+    },
+  ],
+  Milestone: {
+    id: 1,
+    title: 'asdf',
+    content: '수정 테스트',
+    deadline: '2020-11-05',
+    openCount: 10,
+    totalCount: 100,
+    is_opened: true,
+  },
+  Labels: [
+    {
+      id: 1,
+      color: 'red',
+      title: 'asdf',
+      content: '입력 테스트',
+    },
+    {
+      id: 2,
+      color: 'green',
+      title: 'asdf',
+      content: '입력 테스트',
+    },
+  ],
+};
 
 const IssueSidebar = () => {
-  return <div>hello</div>;
+  return (
+    <Container>
+      <DropdownContainer>
+        <AssigneeContainer assignees={issue.Assignees} />
+      </DropdownContainer>
+      <DropdownContainer>
+        <LabelContainer labels={issue.Labels} />
+      </DropdownContainer>
+      <DropdownContainer>
+        <MilestoneContainer milestone={issue.Milestone} />
+      </DropdownContainer>
+    </Container>
+  );
 };
 
 export default IssueSidebar;
