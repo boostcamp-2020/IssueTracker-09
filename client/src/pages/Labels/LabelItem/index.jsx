566ac84b164513305e7b1e19e7f414d3a89d94b6
박은식 <qkrdmstlr3@naver.com>
2020-11-11 18:46:00 +0900

    refactor: label을 받아오는 부분 변경
    - 라벨 생성할 때 전체리렌더링을 시켜주기 위해서 받아오는 부분을 라벨페이지의 최상위 부분으로 변경하였다

--- client/src/pages/Labels/LabelItem/index.jsx
+++ client/src/pages/Labels/LabelItem/index.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable no-unused-vars */
 import React, { useState } from 'react';
-import EditLabel from '../EditLabel';
+import EditLabel from '../../../components/EditLabel';
 import { FlexDiv, LabelIcon, Description, ButtonDiv, Span } from './styled';
 import { updateLabelAPI, deleteLabelAPI } from '../../../apis/label';
 
