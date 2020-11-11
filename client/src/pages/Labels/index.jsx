64d6c6338fe71706b4247dbeba0dde3edb2f1938
jaeik <wodlr2007@naver.com>
2020-11-11 17:49:07 +0900

    feat: 라벨 마일스톤 navform 구현
    - 라벨과 마일스톤의 공통적인 navigator form을 구현함

--- client/src/pages/Labels/index.jsx
+++ client/src/pages/Labels/index.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import LabelList from './LabelList';
+import LabelMileNavForm from '../../components/LabelMileNavForm'
 
 const Labels = () => {
-  return <LabelList />;
+  return (
+    <>
+      <LabelMileNavForm title="Label" />
+      <LabelList />
+    </>
+  );
 };
 
 export default Labels;
