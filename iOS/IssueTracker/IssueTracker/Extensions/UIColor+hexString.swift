91395e7d1b255029f28e25293e14ad2f5910e445
KYHyeon <run190822@gmail.com>
2020-11-12 11:27:16 +0900

    feat: UIColor를 16진수 문자열로 변경하는 기능 추가

--- iOS/IssueTracker/IssueTracker/Extensions/UIColor+hexString.swift
+++ iOS/IssueTracker/IssueTracker/Extensions/UIColor+hexString.swift
@@ -30,4 +30,27 @@ extension UIColor {
         self.init(red: CGFloat(red) / 255.0, green: CGFloat(green) / 255.0, blue: CGFloat(blue) / 255.0, alpha: 1.0)
     }
     
+    convenience init?(hexString: String) {
+        let r, g, b: CGFloat
+        
+        if hexString.hasPrefix("#") {
+            let start = hexString.index(hexString.startIndex, offsetBy: 1)
+            let hexColor = String(hexString[start...])
+            
+            if hexColor.count == 6 {
+                let scanner = Scanner(string: hexColor)
+                var hexNumber: UInt64 = 0
+                
+                if scanner.scanHexInt64(&hexNumber) {
+                    r = CGFloat((hexNumber & 0xff0000) >> 16) / 255
+                    g = CGFloat((hexNumber & 0x00ff00) >> 8) / 255
+                    b = CGFloat((hexNumber & 0x0000ff)) / 255
+                    self.init(red: r, green: g, blue: b, alpha: 1.0)
+                    return
+                }
+            }
+        }
+        
+        return nil
+    }
 }
