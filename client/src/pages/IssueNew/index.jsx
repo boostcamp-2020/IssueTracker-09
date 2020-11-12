ebadd769aafb70bda170ede73a74510435a51d99
Jae-Woo Joo <wnwodn4293@naver.com>
2020-11-12 13:44:07 +0900

    feat: 이슈 생성 페이지 구현
    - 이슈 생성 페이지 레이아웃 구현
    - 이슈 생성 페이지 css 적용
    - 이슈 생성 함수 구현

--- client/src/pages/IssueNew/index.jsx
+++ client/src/pages/IssueNew/index.jsx
@@ -1,7 +1,43 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { useHistory } from 'react-router-dom';
+import NewComment from '../../components/NewComment';
+import { UserContext } from '../../stores/userStore';
+import { createIssueAPI } from '../../apis/issue';
+import { createCommentAPI } from '../../apis/comment';
+import { Container, CommentContainer, SideBarConatiner, Image } from './styled';
 
 const IssueNew = () => {
-  return <div>hello</div>;
+  const {
+    userState: { image },
+  } = useContext(UserContext);
+  const history = useHistory();
+
+  const submitHandler = async ({ title, comment }) => {
+    // const result = await createIssueAPI({
+    //   title,
+    //   labelId: [3],
+    //   assigneeId: [1],
+    //   milestoneId: 1,
+    // });
+    // if (result) {
+    //   if (comment) {
+    //     await createCommentAPI(comment, result.id);
+    //   }
+    //   history.push(`/issues/${result.id}`);
+    // } else {
+    history.push(`/`);
+    // }
+  };
+
+  return (
+    <Container>
+      <CommentContainer>
+        <Image image={image} />
+        <NewComment submitEvent={submitHandler} />
+      </CommentContainer>
+      <SideBarConatiner>123</SideBarConatiner>
+    </Container>
+  );
 };
 
 export default IssueNew;
