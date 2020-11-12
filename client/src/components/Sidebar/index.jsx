bd0be025e4e9b28aec2af3d88d110c5d655a2aa0
박은식 <qkrdmstlr3@naver.com>
2020-11-12 12:58:52 +0900

    refactor: sidebar컴포넌트에 재사용성을 더함
    - sidebar컴포넌트가 재사용이 가능하도록 하였음
    - 따라서 create시에는 API요청을 보내지 않음

--- client/src/components/Sidebar/index.jsx
+++ client/src/components/Sidebar/index.jsx
@@ -4,21 +4,30 @@ import AssigneeContainer from './Assignee';
 import LabelContainer from './Label';
 import MilestoneContainer from './Milestone';
 
-const IssueSidebar = ({ issue }) => {
+const IssueSidebar = ({ issue = { id: 1000 }, type }) => {
   if (!issue.id) {
     return null;
   }
 
+  console.log('!!');
   return (
     <Container>
       <DropdownContainer>
-        <AssigneeContainer assignees={issue.Assignees} issueId={issue.id} />
+        <AssigneeContainer
+          assignees={issue.Assignees}
+          issueId={issue.id}
+          type={type}
+        />
       </DropdownContainer>
       <DropdownContainer>
-        <LabelContainer labels={issue.Labels} issueId={issue.id} />
+        <LabelContainer labels={issue.Labels} issueId={issue.id} type={type} />
       </DropdownContainer>
       <DropdownContainer>
-        <MilestoneContainer milestone={issue.Milestone} issueId={issue.id} />
+        <MilestoneContainer
+          milestone={issue.Milestone}
+          issueId={issue.id}
+          type={type}
+        />
       </DropdownContainer>
     </Container>
   );
